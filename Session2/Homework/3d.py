68860a1a5e64d2db698bfa6a5816bcd65b3d375f
Tuan Luong <a6quocoai@gmail.com>
2018-03-08 05:36:24 +0700

    update

--- Session2/Homework/3d.py
+++ Session2/Homework/3d.py
@@ -1,4 +1,8 @@
 n = int(input("Enter number: "))
+for i in range(n):
+    for j in range(n):
+        print(1 - ((i+j)% 2), end=' ')
+    print()
 # if n % 2 == 0:
 #     for l in range(1, int(n/2) + 1):
 #         for i in range(1, int(n/2)+1):
@@ -10,8 +14,8 @@ n = int(input("Enter number: "))
 #         #     for p in range(1, -1, -1):
 #         #         for q in range(1, 2):
 #         #             print(q*p, end="\t")
-for l in range(1, n + 1):
-    for i in range(4):
-        print("1","\t","0", end = "\t")
-    for k in range(4):
-        print("0","\t","1", end = "\t")
+# for l in range(1, n + 1):
+#     for i in range(4):
+#         print("1","\t","0", end = "\t")
+#     for k in range(4):
+#         print("0","\t","1", end = "\t")
